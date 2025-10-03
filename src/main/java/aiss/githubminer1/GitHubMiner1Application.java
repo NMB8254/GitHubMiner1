71d3bbcb33cf64bf072e746417b679f69cbdb356
NMB8254 <joaquincr72005@gmail.com>
2025-10-03 13:39:59 +0200

    Initial commit

---
+++ src/main/java/aiss/githubminer1/GitHubMiner1Application.java
@@ -0,0 +1,13 @@
+package aiss.githubminer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubMiner1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubMiner1Application.class, args);
+	}
+
+}
---
+++ src/test/java/aiss/githubminer1/GitHubMiner1ApplicationTests.java
@@ -0,0 +1,13 @@
+package aiss.githubminer1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubMiner1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
